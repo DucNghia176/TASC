a858f96b441f51543b6a99bd44dbecc0df780819
DucNghia176 <nghi7a@gmail.com>
2024-10-16 14:45:53 +0700

    TASC traning

--- src/JavaCore/String/StringDemo.java
+++ src/JavaCore/String/StringDemo.java
@@ -1,4 +1,16 @@
 package JavaCore.String;
 
 public class StringDemo {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = new String("hello");
+
+        System.out.println("s1==s2: "+(s1 == s2));
+        System.out.println("s1==s3: "+(s1 == s3));
+
+        System.out.println("s1.equals(s3): "+s1.equals(s3));
+        String s4 = "HELLO";
+        System.out.println("s1.equalsIgnoreCase(s4): "+s1.equalsIgnoreCase(s4));
+    }
 }
